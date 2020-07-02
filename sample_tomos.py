68ed27309d7b5b79727442207b95aedd5fda8be4
ken kono <k.kouno@kiyono-co.jp>
2020-07-02 17:45:17 +0900

    add store info

--- sample_tomos.py
+++ sample_tomos.py
@@ -5,8 +5,8 @@ import random
 
 fake = Faker()
 
-start_date = datetime.date(year=2019, month=1, day=1)
-end_date = datetime.date(year=2019, month=12, day=31)
+start_date = datetime.date(year=2019, month=6, day=1)
+end_date = datetime.date(year=2019, month=6, day=30)
 
 def customerDataGenerate(records, customerHeaders):
   fake = Faker('ja_JP')
@@ -28,7 +28,8 @@ def customerDataGenerate(records, customerHeaders):
         })
 
 def storeDataGenerate(records, storeHeaders):
-  stores = ["a", "b", "c", "d", "e"]
+  # stores = ["トモズ 秋葉原店", "トモズ 大手町 カンファレンスセンター店", "トモズ 大手町プレイス店", "トモズ KITTE店", "トモズ 東京ミッドタウン日比谷店", "トモズ 神田和泉町店", "トモズ 晴海トリトン店", "トモズ銀座三丁目店", "トモズ トルナーレ浜町店", "トモズ コレド日本橋店", "トモズ 水天宮前店", "トモズ 茅場町店", "トモズ 品川インターシティ店", "トモズ 浜松町店", "トモズエクスプレス グランゲート新橋店", "トモズ 汐留シティセンター店", "トモズ 赤坂店", "トモズ アークヒルズ店", "トモズ 六本木ヒルズ店", "トモズ 麻布十番店", "トモズ 白金プラザ店", "トモズ 東京ミッドタウン店", "トモズ 神谷町店", "トモズ 虎ノ門ヒルズ店", "トモズ 新宿二丁目店", "トモズ 東京オペラシティ店", "トモズ 西新宿五丁目店", "トモズ 女子医大前店", "トモズ 女子医大前2号店", "トモズ 新宿住友ビル店", "トモズ 高田馬場店", "トモズ EQUiA曳舟店", "トモズ 向島店", "トモズ 有明ガーデン店", "トモズ 亀戸東口店", "トモズ 南砂町SUNAMO店", "トモズ ゲートシティ大崎店"]
+  stores = ["トモズ 秋葉原店", "トモズ 大手町 カンファレンスセンター店", "トモズ 大手町プレイス店", "トモズ KITTE店", "トモズ 東京ミッドタウン日比谷店", "トモズ 神田和泉町店"]
   with open("store_data.csv", 'wt') as csvFile:
     writer = csv.DictWriter(csvFile, fieldnames=storeHeaders)
     writer.writeheader()
