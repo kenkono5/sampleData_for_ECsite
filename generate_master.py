d588f35642f4aaa12f3dd7fef52469ae697dd322
ken kono <k.kouno@kiyono-co.jp>
2020-07-10 17:14:59 +0900

    prefecture

--- generate_master.py
+++ generate_master.py
@@ -19,7 +19,7 @@ while t <= end_dt:
   t += datetime.timedelta(days=1)
 month_array = [x.strftime("%Y-%m-%d") for x in lst]
 
-def categoryDataGenerate(records, categoryHeaders):
+def categoryDataGenerate(categoryHeaders):
   categorys = ["日用品", "医薬品", "食品", "衛生用品", "化粧品"]
   with open("category_data.csv", 'wt', encoding='utf_8_sig') as csvFile:
     writer = csv.DictWriter(csvFile, fieldnames=categoryHeaders)
@@ -32,9 +32,24 @@ def categoryDataGenerate(records, categoryHeaders):
         })
       id+=1
 
+def prefectureDataGenerate(prefectureHeaders):
+  prefectures = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
+  with open("prefecture_data.csv", 'wt', encoding='utf_8_sig') as csvFile:
+    writer = csv.DictWriter(csvFile, fieldnames=prefectureHeaders)
+    writer.writeheader()
+    id = 1
+    for i in prefectures:
+      writer.writerow({
+        "PrefectureID": id,
+        "Prefecture": i,
+        })
+      id+=1
+
 if __name__ == '__main__':
-  category_records = 1000
   categoryHeaders = ["CategoryID", "Category"]
-  categoryDataGenerate(category_records, categoryHeaders)
+  categoryDataGenerate(categoryHeaders)
+
+  prefectureHeaders = ["PrefectureID", "Prefecture"]
+  prefectureDataGenerate(prefectureHeaders)
 
   print("CSV generation complete!")
